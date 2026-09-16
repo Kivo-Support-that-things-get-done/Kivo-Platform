0a25966f0c29da306672ca40a1e3cc839d479603
tanushree-l <tanushreel2003@gmail.com>
2026-09-17 00:32:06 +0530

    chore: initialize Spring Boot project

---
+++ src/main/java/com/kivo/KivoApplication.java
@@ -0,0 +1,13 @@
+package com.kivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kivo/KivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
